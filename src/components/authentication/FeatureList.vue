e34c3c81f7d23b3bc68de4f8637893e52daabc13
sainrahmanii <husainrahmani127@gmail.com>
2022-12-11 11:42:42 +0700

    add register

---
+++ src/components/authentication/FeatureList.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Customizable from "../authentication/features/Customizable.vue";
+import Fortune from "../authentication/features/Fortune.vue";
+import Documentation from "../authentication/features/Documentation.vue";
+</script>
+
+<template>
+  <Customizable />
+  <Fortune />
+  <Documentation />
+</template>
